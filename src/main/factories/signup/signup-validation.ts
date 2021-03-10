87129c53c2be52d78793c3eb1650d964472bd64d
Leonardo Braz <lhleonardo05@gmail.com>
2021-03-10 18:32:57 -0300

    refactor: move password comparation to the Validation Composite

--- src/main/factories/signup/signup-validation.ts
+++ src/main/factories/signup/signup-validation.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
 import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
@@ -7,6 +8,7 @@ export const makeSignupValidation = (): Validation => {
     new RequiredFieldValidation('name'),
     new RequiredFieldValidation('email'),
     new RequiredFieldValidation('password'),
-    new RequiredFieldValidation('passwordConfirmation')
+    new RequiredFieldValidation('passwordConfirmation'),
+    new CompareFieldsValidation('password', 'passwordConfirmation')
   ])
 }
---
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -0,0 +1,23 @@
+import { InvalidParamError } from '../../errors'
+import { Validation } from './validation'
+
+export class CompareFieldsValidation implements Validation {
+  private readonly source: string
+  private readonly target: string
+
+  constructor (source: string, target: string) {
+    this.source = source
+    this.target = target
+  }
+
+  public validate (input: any): Error {
+    const sourceValue = input[this.source]
+    const targetValue = input[this.target]
+
+    if (sourceValue !== targetValue) {
+      return new InvalidParamError(this.target)
+    }
+
+    return null
+  }
+}
