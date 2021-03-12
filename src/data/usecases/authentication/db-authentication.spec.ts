df15face56e459dba289bf824f4cf62cfa8756da
Leonardo Braz <lhleonardo05@gmail.com>
2021-03-11 22:26:00 -0300

    test: ensure DbAuthentication returns null if not found user by email

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -54,4 +54,14 @@ describe('DbAuthentication UseCase', () => {
 
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve) => resolve(null)))
+
+    const accessToken = await sut.auth(makeFakeCredentials())
+
+    expect(accessToken).toBe(null)
+  })
 })
