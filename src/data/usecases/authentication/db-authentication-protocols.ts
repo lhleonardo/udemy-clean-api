5453114132fb83a4aeac980b1bad312cc3977727
Leonardo Braz <lhleonardo05@gmail.com>
2021-03-11 23:26:16 -0300

    feat: ensure DbAuthentication returns token if succeeds

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -1,4 +1,5 @@
-export * from '../../protocols/db/load-account-by-email-repository'
 export * from '../../../domain/usecases/authentication'
 export * from '../../protocols/criptography/hash-comparer'
 export * from '../../protocols/criptography/token-generator'
+export * from '../../protocols/db/load-account-by-email-repository'
+export * from '../../protocols/db/update-access-token-repository'
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,24 +1,29 @@
-import { LoadAccountByEmailRepository, Authentication, AuthenticationModel, HashComparer, TokenGenerator } from './db-authentication-protocols'
+import { Authentication, AuthenticationModel, HashComparer, LoadAccountByEmailRepository, TokenGenerator, UpdateAccessTokenRepository } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmail: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGenerator: TokenGenerator
+  private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
 
-  constructor (hashComparer: HashComparer, loadAccountByEmail: LoadAccountByEmailRepository, tokenGenerator: TokenGenerator) {
+  constructor (hashComparer: HashComparer, loadAccountByEmail: LoadAccountByEmailRepository, tokenGenerator: TokenGenerator, updateAccessTokenRepository: UpdateAccessTokenRepository) {
     this.hashComparer = hashComparer
     this.loadAccountByEmail = loadAccountByEmail
     this.tokenGenerator = tokenGenerator
+    this.updateAccessTokenRepository = updateAccessTokenRepository
   }
 
   async auth (credentials: AuthenticationModel): Promise<string> {
     const user = await this.loadAccountByEmail.load(credentials.email)
 
     if (user) {
-      const compareResult = await this.hashComparer.compare(credentials.password, user.password)
+      const isValid = await this.hashComparer.compare(credentials.password, user.password)
 
-      if (compareResult) {
-        await this.tokenGenerator.generate(user.id)
+      if (isValid) {
+        const accessToken = await this.tokenGenerator.generate(user.id)
+        await this.updateAccessTokenRepository.update(user.id, accessToken)
+
+        return accessToken
       }
     }
 
